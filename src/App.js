f1c024b2c6285f26f362c14561905e9103fee302
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2021-10-29 17:00:45 -0300

    button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import styled from 'styled-components'
-
 import { Formik, Form } from 'formik'
 
 import Input from './components/Input'
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
-
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
-`
+import Button from './components/Button'
+import Section from './components/Section'
+import Container from './components/Container'
 
 const App = () => {
   const handleSubmit = () => {}
@@ -39,6 +25,7 @@ const App = () => {
             <Input name="contribution" label="Annual Contribution" />
             <Input name="years" label="Years" />
             <Input name="rate" label="Estimated Interest" />
+            <Button>Calculate</Button>
           </Form>
         </Formik>
       </Section>
